cd22b2ea6f3ce18f7e33bf6140878da38cb780a5
Luz Mashas <luzmashas@Luzs-MacBook-Pro.local>
2018-02-10 07:56:45 -0500

    Heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friendTable = [
   {
     personFullName: "John Smith",
-    personPhotourl: "http://www.flickr.com/johndoe",
+    personPhotourl: "https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_1280.png",
     personScores: ['5','1','2','3','1','2','1','5','2','3']
   },
   {
     personFullName: "Kyle Brown",
-    personPhotourl: "https://pixabay.com/en/man-person-avatar-face-head-156584/",
+    personPhotourl: "https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png",
     personScores: ['4','1','2','3','2','2','1','3','2','3']
   }
 ];
