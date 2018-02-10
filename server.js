c9e7f59428e200eeec2fe9cda7bb1181cecad415
Luz Mashas <luzmashas@Luzs-MacBook-Pro.local>
2018-02-10 15:53:50 -0500

    image update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -9,6 +10,8 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname,"app/public")));
 
 // ================================================================================
 // ROUTER
